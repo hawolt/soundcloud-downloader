9387f17500ebd489d6652dc277b3a07696b1dd90
hawolt <hawolton@gmail.com>
2022-02-09 15:46:15 +0100

    TrackLoader for threaded loading

---
+++ src/main/java/com/hawolt/data/media/TrackLoader.java
@@ -0,0 +1,28 @@
+package com.hawolt.data.media;
+
+import com.hawolt.Request;
+import com.hawolt.Response;
+import org.json.JSONArray;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Created: 09.02.2022 15:33
+ * Author: Twitter @hawolt
+ **/
+
+public class TrackLoader implements Callable<Track> {
+
+    private final String resource;
+
+    public TrackLoader(String resource) {
+        this.resource = resource;
+    }
+
+    @Override
+    public Track call() throws Exception {
+        Request request = new Request(resource);
+        Response response = request.execute();
+        return new Track(new JSONArray(response.getBodyAsString()).getJSONObject(0));
+    }
+}
