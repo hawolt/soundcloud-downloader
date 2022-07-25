caafd83c7c00974465eb2ef8acee6b9f878632a3
hawolt <hawolton@gmail.com>
2022-07-25 09:20:26 +0200

    allowing to register multiple HydratableInterfaces

--- src/main/java/com/hawolt/data/media/Soundcloud.java
+++ src/main/java/com/hawolt/data/media/Soundcloud.java
@@ -1,7 +1,6 @@
 package com.hawolt.data.media;
 
 import com.hawolt.Logger;
-import com.hawolt.Request;
 import com.hawolt.Response;
 import com.hawolt.data.media.download.DownloadCallback;
 import com.hawolt.data.media.hydratable.Hydratable;
@@ -13,7 +12,6 @@ import com.hawolt.data.media.impl.MediaTrackInterface;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,7 +28,7 @@ public class Soundcloud {
     public static long limit = 20;
 
     private static final Map<String, MediaInterface<? extends Hydratable>> MAPPING = new HashMap<>();
-    private static final Map<Class<? extends Hydratable>, HydratableInterface<? extends Hydratable>> MANAGER = new HashMap<>();
+    private static final Map<Class<? extends Hydratable>, List<HydratableInterface<? extends Hydratable>>> MANAGER = new HashMap<>();
 
     static {
         MAPPING.put("playlist", new MediaPlaylistInterface());
@@ -38,8 +36,13 @@ public class Soundcloud {
         MAPPING.put("sound", new MediaTrackInterface());
     }
 
+    public static void overwrite(String type, MediaInterface<? extends Hydratable> mediaInterface) {
+        MAPPING.put(type, mediaInterface);
+    }
+
     public static void register(Class<? extends Hydratable> clazz, HydratableInterface<? extends Hydratable> manager) {
-        MANAGER.put(clazz, manager);
+        if (!MANAGER.containsKey(clazz)) MANAGER.put(clazz, new ArrayList<>());
+        MANAGER.get(clazz).add(manager);
     }
 
     @SuppressWarnings("all")
@@ -54,8 +57,8 @@ public class Soundcloud {
     static void load(String link, DownloadCallback callback) {
         Hydratable.EXECUTOR_SERVICE.execute(() -> {
             try {
-                Request request = new Request(link);
-                Response response = request.execute();
+                MediaLoader loader = new MediaLoader(link);
+                Response response = loader.call();
                 JSONArray hydration = Hydration.from(response.getBodyAsString());
                 Map<String, JSONObject> available = new HashMap<>();
                 for (int i = 0; i < hydration.length(); i++) {
@@ -73,9 +76,11 @@ public class Soundcloud {
                 CompletableFuture.supplyAsync(() -> MAPPING.get(hydratable).convert(available.get(hydratable))).whenComplete((capture, e) -> {
                     if (e != null) Logger.error(e);
                     if (capture == null) return;
-                    MANAGER.get(capture.getClass()).accept(modify(capture));
+                    for (HydratableInterface<? extends Hydratable> hydratableInterface : MANAGER.get(capture.getClass())) {
+                        hydratableInterface.accept(modify(capture));
+                    }
                 });
-            } catch (IOException e) {
+            } catch (Exception e) {
                 if (callback == null) Logger.error(e.getMessage());
                 else callback.onLoadFailure(link, e);
             }
