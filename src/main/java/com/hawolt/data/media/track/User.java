cda5589fcf27c180d167759532779c0522194998
hawolt <hawolton@gmail.com>
2022-12-16 05:17:51 +0100

    added username field to user

--- src/main/java/com/hawolt/data/media/track/User.java
+++ src/main/java/com/hawolt/data/media/track/User.java
@@ -9,10 +9,15 @@ import org.json.JSONObject;
 
 public class User {
 
-    private final String permalink;
+    private final String permalink, username;
 
     public User(JSONObject o) {
         this.permalink = !o.isNull("permalink") ? o.getString("permalink") : "";
+        this.username = !o.isNull("username") ? o.getString("username") : "";
+    }
+
+    public String getUsername() {
+        return username;
     }
 
     public String getPermalink() {
