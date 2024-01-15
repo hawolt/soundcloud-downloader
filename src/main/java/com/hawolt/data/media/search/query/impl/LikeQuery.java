8e911544bf6d7954ea449f81efdc219438bb43d8
hawolt <45050975+hawolt@users.noreply.github.com>
2024-01-15 13:21:18 +0100

    add query file

---
+++ src/main/java/com/hawolt/data/media/search/query/impl/LikeQuery.java
@@ -0,0 +1,48 @@
+package com.hawolt.data.media.search.query.impl;
+
+import com.hawolt.cryptography.SHA256;
+import com.hawolt.data.media.hydratable.impl.track.Track;
+import com.hawolt.data.media.search.query.Query;
+import org.json.JSONObject;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Created: 13/11/2022 19:17
+ * Author: Twitter @hawolt
+ **/
+
+public class LikeQuery implements Query<Track> {
+    private final long timestamp;
+    private final long userId;
+
+    public LikeQuery(long timestamp, long userId) {
+        this.timestamp = timestamp;
+        this.userId = userId;
+    }
+
+    public LikeQuery(long userId) {
+        this(System.currentTimeMillis(), userId);
+    }
+
+    @Override
+    public String getKeyword() {
+        return String.valueOf(userId);
+    }
+
+    @Override
+    public String checksum() {
+        return SHA256.hash(String.join(getClass().getSimpleName(), String.valueOf(userId)));
+    }
+
+    @Override
+    public Predicate<Track> filter() {
+        return track -> true;
+    }
+
+    @Override
+    public Function<JSONObject, Track> getTransformer() {
+        return object -> new Track(timestamp, object.getJSONObject("track"));
+    }
+}
